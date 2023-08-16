5543fe8228b5c24a985551f4034c0683e850c8e6
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2023-08-16 21:36:48 +0530

    first commit

---
+++ blazepose_demo.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = pose.process(rgb_frame)
+
+    if results.pose_landmarks:
+        for landmark in results.pose_landmarks.landmark:
+            x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+            cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    cv2.imshow('BlazePose Keypoints', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
